a3fca1481988185b4b046de25df255a241a87240
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-21 02:54:16 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Francesca MACDONALD on 2023-08-21.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
